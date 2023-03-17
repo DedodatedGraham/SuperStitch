d161486d3b5a53481cad1c034888100d00251ccd
Garcia <Graham_Garcia1@baylor.edu>
2023-03-17 16:05:11 -0500

    New Stitch

--- src/SuperStitch.m
+++ src/SuperStitch.m
@@ -1,2 +1,14 @@
+function []  = untitled(inputPath,N)
+%Super Stitch input 
+%Input Path will be externally provided and have the path 
+% past SuperStitch/Input
+%N is the amount of photos needed tobe loaded in
+imgCluster = [];
+imgPath = dir('input\inputPath');
+%%
 %Main SuperStitch Code
+for i=1:1:N
+end
 
+%%
+end
