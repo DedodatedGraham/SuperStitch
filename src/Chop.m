0efd7cca308571786d5b18b09a2a065fafeeddac
DedodatedGraham <89348650+DedodatedGraham@users.noreply.github.com>
2023-03-20 07:52:26 -0500

    small edits

--- src/Chop.m
+++ src/Chop.m
@@ -3,23 +3,28 @@ function [] = Chop(inputPath,N,M,O)
 %pictures with a 'O'% overlap
 
 %size of pictures in pixels will be determined based on input size px,py
-imgPath = append(pwd,'\SuperStitch\input\',inputPath);
+imgPath = append(pwd,'\input\',inputPath);
 mainImg = imread(imgPath);
 pos = struct("x",0,"y",0,"w",0,"h",0);
 imgpos(N,M) = pos; 
-imgPart(N,M);
 [px,py,ncc] = size(mainImg);
+%imgPart = zeros(M,N,ncc);
 nx = ceil(px/N);
 ny = ceil(py/M);
-onx = nx * O * .01;
-ony = ny * O * .01;
+onx = ceil(nx * O * .01);
+ony = ceil(ny * O * .01);
 %assign pics
 for i=1:1:N
     for j=1:1:M
-        %Assign non adjusted 
+        %Assign non adjusted box 
         imgpos(i,j).x = nx * i;
         imgpos(i,j).y = ny * j;
-           
+        imgpos(i,j).w = nx;
+        imgpos(i,j).h = ny;
+        %Apply overlap if nt on edge
+        %Finally output image
+        temp = mainImg(imgpos(i,j).x : imgpos(i,j).x + imgpos(i,j).w,imgpos(i,j).y : imgpos(i,j).y + imgpos(i,j).h,:);
+        %slicepath = append(pwd,'\input\brokenImg\',num2str(imgpos(i,j).x),'-',num2str(imgpos(i,j).y),'.png');
+        %imwrite(temp,slicepath);
     end
 end
-nx;
