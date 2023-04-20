bb39f8e459be7db11316948e5992892bc7f0790e
DedodatedGraham <grahamg7584@gmail.com>
2023-04-20 15:48:55 -0500

    quickcommit

--- src/launch_camera.sh
+++ src/launch_camera.sh
@@ -2,7 +2,7 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 cd camera/
 CAMBINARY=camrunner
-if [! -f"$CAMBINARY" ];
+if [-f"$CAMBINARY" ];
 then
     echo 'already exists'
 else
