ee4808a892678e7959e5a81bcccd59f8233a4aa9
DedodatedGraham <grahamg7584@gmail.com>
2023-04-20 15:45:25 -0500

    quickcommit

---
+++ src/clean.sh
@@ -0,0 +1,3 @@
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+./camera/clean.sh
---
+++ src/launch_camera.sh
@@ -0,0 +1,11 @@
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+cd camera/
+CAMBINARY=camrunner
+if [! -f"$CAMBINARY" ];
+then
+    echo 'already exists'
+else
+    make
+fi
+./$CAMBINARY
