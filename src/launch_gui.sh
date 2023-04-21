119a54388fe4c373ffae3ae669a88b4f730861bd
DedodatedGraham <grahamg7584@gmail.com>
2023-04-20 22:47:48 -0500

    quickcommit

--- src/launch_gui.sh
+++ src/launch_gui.sh
@@ -6,6 +6,6 @@ this_root=$1
 micro_controller=$2
 echo "Launching GUI"
 #sshpass -p machinevision ssh debian@192.168.7.2 "source ~/.bashrc;$NODE_PATH"
-sshpass -p machinevision ssh debian@192.168.7.2 -t 'source ~/.bashrc; source ~/.profile; $NODE_PATH ;cd /var/lib/cloud9/stageTranslation;./run_gui.sh' &
+sshpass -p machinevision ssh debian@192.168.7.2 -t "source ~/.bashrc; source ~/.profile; $NODE_PATH ;cd /var/lib/cloud9/stageTranslation;./run_gui.sh $" &
 sleep 15
 xdg-open http://192.168.7.2:8085/machinevision.html
--- src/launch_matlab.sh
+++ src/launch_matlab.sh
@@ -3,4 +3,4 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 #Get input args
 this_root=$1
-micro_controller=$2
+:
